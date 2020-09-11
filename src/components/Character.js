0ac2e27a22977c26ec2105483c479af850ff976f
Nathaniel Patterson <odst0016@gmail.com>
2020-09-11 12:06:48 -0400

    MVP done and some stretch

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import CharacterInfo from "./CharacterInfo";
 
 export default function Character(props) {
--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 
 export default function CharacterInfo(props) {
   const { apiData } = props;
