356f1c9fa237512bf3648744346532f3d7e9e2ee
Nathaniel Patterson <odst0016@gmail.com>
2020-09-11 11:52:29 -0400

    MVP done and some stretch

--- src/components/Character.js
+++ src/components/Character.js
@@ -7,7 +7,9 @@ export default function Character(props) {
   return (
     <div>
       <h2>{apiData.name}</h2>
+      <hr />
       <img alt={apiData.name} src={apiData.image}></img>
+      <hr />
       <CharacterInfo apiData={apiData} />
     </div>
   );
